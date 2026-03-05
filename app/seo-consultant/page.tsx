998669c89a80aab6d32cd6dce6293e678ce2e7bb
paramjani <125356533+paramjani@users.noreply.github.com>
2026-03-05 18:09:32 +0530

    Update page.tsx

--- app/seo-consultant/page.tsx
+++ app/seo-consultant/page.tsx
@@ -213,7 +213,7 @@ export default function SeoConsultant() {
       {/* FLOATING WHATSAPP */}
 
       <a
-        href="https://wa.me/919876543210"
+        href="https://wa.me/917862005679"
         target="_blank"
         rel="noopener noreferrer"
         className="fixed bottom-6 right-6 bg-[#25D366] text-white px-4 py-3 rounded-full shadow-lg hover:scale-110 transition"
